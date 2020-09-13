d6b3a2067a2f499ad7373b7da3e62220d28d34a5
MykytaMaliarenko <mykytamaliarenko@gmail.com>
2020-09-13 23:14:55 +0300

    1 lab:  ref CSVParser: fix issue #2 and #3

--- 1/src/com/maliarenko/mykyta/CSVParser.java
+++ 1/src/com/maliarenko/mykyta/CSVParser.java
@@ -41,34 +41,51 @@ public class CSVParser {
         StringBuilder result = new StringBuilder();
 
         while (true) {
-            // We reached last value in this line
-            if (!hasMoreValues(inputCopy)) {
-                result.append(String.format("%d", inputCopy.length()));
+            String block = "";
+            int endIndex = this.getCSVBlockEndIndex(inputCopy);
+            if (inputCopy.charAt(0) == '"') {
+                block = inputCopy.substring(1, endIndex);
+                endIndex += 2;
+            } else if (inputCopy.contains(",")) {
+                endIndex = inputCopy.indexOf(',') + 1;
+                block = inputCopy.substring(0, endIndex - 1);
+            }
+
+            if (endIndex != -1 && endIndex < inputCopy.length()) {
+                result.append(String.format("%d%s", block.length(), outputDelimiter));
+                inputCopy = inputCopy.substring(endIndex);
+            } else {
+                // We reached last value in this line
+                result.append(String.format("%d", inputCopy.length() - (inputCopy.charAt(0) == '"' ? 2 : 0)));
                 break;
             }
-
-            String block;
-            int endIndex;
-            if (inputCopy.charAt(0) == '"') {
-                endIndex = inputCopy.indexOf('"', 1) + 1;
-                if (endIndex == 0)
-                    throw new IllegalArgumentException("no closing \"");
-
-                block = inputCopy.substring(1, endIndex - 1);
-            } else {
-                endIndex = inputCopy.indexOf(',');
-                block = inputCopy.substring(0, endIndex);
-            }
-
-            result.append(String.format("%d%s", block.length(), outputDelimiter));
-            inputCopy = inputCopy.substring(endIndex + 1);
         }
 
         return result.toString();
     }
 
-    private boolean hasMoreValues(String input) {
-        return input.contains(",") || input.charAt(0) == '"';
+    private int getCSVBlockEndIndex(String input) {
+        if (input.charAt(0) == '"') {
+            long numOfQuotes = input.chars().filter(ch -> ch == '"').count();
+            if (numOfQuotes%2 != 0)
+                throw new IllegalArgumentException("no closing \"");
+
+            String temp = input.substring(1);
+            int cutPartLength = 1;
+            int quoteNum = 1;
+            while (true) {
+                int quoteIndex = temp.indexOf('"');
+                if (quoteIndex == temp.length() - 1 || (quoteNum%2 != 0 && temp.charAt(quoteIndex + 1) == ','))
+                    return quoteIndex + cutPartLength;
+                else {
+                    quoteNum++;
+                    cutPartLength += quoteIndex + 1;
+                    temp = temp.substring(quoteIndex + 1);
+                }
+            }
+        } else {
+            return input.indexOf(",");
+        }
     }
 
     public String getInputDelimiter() {
