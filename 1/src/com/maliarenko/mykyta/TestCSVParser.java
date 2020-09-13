aec6fb7b4389d7a90bb34f685e835a6c5a03b341
MykytaMaliarenko <mykytamaliarenko@gmail.com>
2020-09-13 23:16:33 +0300

    1 lab:  feat TestCSVParser: add new tests

--- 1/src/com/maliarenko/mykyta/TestCSVParser.java
+++ 1/src/com/maliarenko/mykyta/TestCSVParser.java
@@ -13,8 +13,14 @@ public class TestCSVParser {
 
     @Test
     public void testParseString() {
-        assertEquals(this.instance.parseString("a,b,c"), "1+1+1");
-        assertEquals(this.instance.parseString("a,\"b,c\",c"), "1+3+1");
+        assertEquals("1+1+1", this.instance.parseString("a,b,c"));
+        assertEquals("1+3+1", this.instance.parseString("a,\"b,c\",c"));
+        assertEquals("2+2+0+11", this.instance.parseString("11,AU,\"\",Aus\"\"tralia"));
+        assertEquals("2+2+11", this.instance.parseString("\"13\",\"AU\",\"Aus\"\"tralia\""));
+        //"12","AU"," ","Australia"
+        assertEquals("2+2+0+9", this.instance.parseString("\"12\",\"AU\",\"\",\"Australia\""));
+        assertEquals("2+2+13+14", this.instance.parseString("\"13\",\"AU\",\"A\"\"us\"\"tralia\"," +
+                "\"A\"\"us,\"\"tralia\""));
     }
 
     @Test(expected = IllegalArgumentException.class)
